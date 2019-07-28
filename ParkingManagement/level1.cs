b68e1c3c3984c4431f818411cd27d8c81150f325
Rifat Hossain Khan <rifat.hk889@gmail.com>
2019-07-28 23:00:27 +0600

    first commit

---
+++ ParkingManagement/level1.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace ParkingManagement
+{
+    public partial class frmlevel1 : Form
+    {
+        public frmlevel1()
+        {
+            InitializeComponent();
+        }
+
+        private void radioButton13_CheckedChanged(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
---
+++ ParkingManagement/parkingcontrol.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace ParkingManagement
+{
+    public partial class frmparkingcontrol : Form
+    {
+        public frmparkingcontrol()
+        {
+            InitializeComponent();
+        }
+
+        private void btnLevel1_Click(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
